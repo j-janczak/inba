57a4971e12e405c5308efc03d68c1107c06197b5
22982835 <kubjan4@gmail.com>
2020-03-19 17:33:20 +0100

    Updated !mi avatar

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,16 +1,35 @@
 const Discord = require('discord.js');
 const op = require(`../my_modules/inbaOutputs.js`);
+const sd = require(`../my_modules/simpleDiscord.js`);
 
 module.exports = {
     name: 'avatar',
     description: 'Shows avatar',
     execute(message, args) {
-        var member = (message.mentions.members.size ? message.mentions.members.first(): message.member);
-        const rolesEmbed = new Discord.RichEmbed()
-            .setTitle(`@${member.displayName}`)
-            .setDescription(op.direct(`avatar`, `user_avatar`))
-            .setColor('#0099ff')
-            .setImage(member.user.avatarURL);
-        message.channel.send(rolesEmbed);
+        let title = ``;
+        let desc = ``;
+        let color = ``;
+        let image = ``;
+
+        if (args[1] == `server`) {
+            title = `Server icon`;
+            desc = `What a beauty!`;
+            color = `#2f3136`;
+            image = message.guild.iconURL;
+        } else {
+            let member = sd.getMember(message, this.name, args[1]);
+            console.log(member);
+            title = `@${member.displayName}`;
+            desc = op.direct(`avatar`, `user_avatar`);
+            color = member.displayHexColor;
+            image = member.user.avatarURL
+        }
+        const iconEmbed = new Discord.RichEmbed()
+            .setTitle(title)
+            .setDescription(desc)
+            .setColor(color)
+            .setImage(image);
+
+        if (iconEmbed != null || iconEmbed !== undefined) message.channel.send(iconEmbed).catch(console.error);
     },
 };
--- my_modules/simpleDiscord.js
+++ my_modules/simpleDiscord.js
@@ -1,16 +1,36 @@
 const Discord = require(`discord.js`);
+const botConfig = require(`../config/config.json`);
+
+function _send(message, str, callback = null) {
+    message.channel.send(str)
+    .then(msg => {if(callback) callback(msg)})
+    .catch(error => {console.error(`Failed to send message!\nserwer: ${message.guild.name}\nchannel:${message.channel.name}\nerror: ${error}`)});
+}
+
+function _getEmbed(type, msg) {
+    let embed = new Discord.RichEmbed();
+    if (type == 0) embed.setDescription(`⛔\xa0\xa0\xa0\xa0${msg}`).setColor(`#D32F2F`);
+    else if (type == 1) embed.setDescription(`✅\xa0\xa0\xa0\xa0${msg}`).setColor(`#00E676`);
+    else if (type == 2) embed.setDescription(`⚠\xa0\xa0\xa0\xa0${msg}`).setColor(`#F57C00`);
+    return embed;
+}
+
+function _getMember(message, command, arg) {
+    let member = undefined;
+    if (message.mentions.users.size) {
+        let memberMentionRegEx = new RegExp(/^<@!(\d+)>$/).exec(arg);
+        if (memberMentionRegEx != null && memberMentionRegEx !== undefined) {
+            member = message.guild.members.find(m => m.user.id == memberMentionRegEx[1]);
+        }
+    } else {
+        let memberNick = message.content.slice(botConfig.prefix.length + 1 + command.length + 1).trim();
+        member = message.guild.members.find(m => m.user.username == memberNick) || message.guild.members.find(m => m.nickname == memberNick);
+    }
+    return member;
+}
 
 module.exports = {
-    send(message, str, callback = null) {
-        message.channel.send(str)
-        .then(msg => {if(callback) callback(msg)})
-        .catch(error => {console.error(`Failed to send message!\nserwer: ${message.guild.name}\nchannel:${message.channel.name}\nerror: ${error}`)});
-    },
-    getEmbed(type, msg) {
-        let embed = new Discord.RichEmbed();
-        if (type == 0) embed.setDescription(`⛔\xa0\xa0\xa0\xa0${msg}`).setColor(`#D32F2F`);
-        else if (type == 1) embed.setDescription(`✅\xa0\xa0\xa0\xa0${msg}`).setColor(`#00E676`);
-        else if (type == 2) embed.setDescription(`⚠\xa0\xa0\xa0\xa0${msg}`).setColor(`#F57C00`);
-        return embed;
-    },
+    send(message, str, callback = null) {return _send(message, str, callback)},
+    getEmbed(type, msg) {return _getEmbed(type, msg)},
+    getMember(message, command, arg) {return _getMember(message, command, arg)}
 };
