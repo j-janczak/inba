ff67ca6403a0a44152b3f88eeaeb87c44ec086f8
22982835 <kubjan4@gmail.com>
2020-05-29 12:48:33 +0200

    [WiP] Logs: server ranking

--- commands/logs.js
+++ commands/logs.js
@@ -1,5 +1,7 @@
 const CommandTemplate = require(`../my_modules/CommandTemplate.js`);
+const botConfig = require(`../config/config.json`);
 const {db} = require(`../my_modules/database.js`);
+const Discord = require(`discord.js`);
 
 class Logs extends CommandTemplate {
     constructor(msg, args) {
@@ -15,6 +17,8 @@ class Logs extends CommandTemplate {
                 if (member) return this.msgCount(member);
                 else this.sendEmbed(0, this.getString(`typical`, `error`, `memberNotFound`));
             }
+        } else if (this.action == `ranking`) {
+            if (this.checkPermission()) return this.ranking();
         }
 
         //let member = this.getMember(2);
@@ -25,6 +29,24 @@ class Logs extends CommandTemplate {
         let countEmbed = this.returnEmbed(3, this.getString(`logs`, `msgCount`, [`<@!${member.id}>`, `\`${count[0].msgCount}\``]));
         countEmbed.setFooter(this.getString(`logs`, `footer`));
         this.send(countEmbed);
+    }
+    async ranking() {
+        let rankingData = await db.query("SELECT DISTINCT userFK as userID, (SELECT count(*) FROM `messageLogs` WHERE `userFK` = userID AND `serverFK` = ?) as msgCount FROM `messageLogs` WHERE `serverFK` = ? AND `bot` = 0 ORDER BY `msgCount` DESC LIMIT 10;", [this.msg.guild.id, this.msg.guild.id]);
+        if (!rankingData) return;
+
+        let rankingEmbed = new Discord.MessageEmbed();
+        rankingEmbed.setTitle(`Top of ${this.msg.guild.name}`);
+        rankingEmbed.setColor(botConfig.botColor);
+        rankingEmbed.setFooter(this.getString(`logs`, `footer`));
+
+        let rankingTxt = ``;
+        rankingData.forEach((member, memberIndex) => {
+            rankingTxt += `\`${memberIndex+1}\`. <@!${member.userID}> - \`${member.msgCount}\`\n`;
+        });
+
+        rankingEmbed.setDescription(rankingTxt);
+
+        this.send(rankingEmbed);
     }
 }
 
