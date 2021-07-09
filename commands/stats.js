d1591f81c3713da570da45953b1c4500a9f28606
22982835 <kubjan4@gmail.com>
2021-07-09 20:06:05 +0200

    xch garden status v2

--- commands/stats.js
+++ commands/stats.js
@@ -12,32 +12,36 @@ class Stats extends CommandTemplate {
         try {
             const statusResult = await axios.get(`https://api.xch.garden/stats`);
 
-            console.log(statusResult.data);
-
-            const formatStr = (str) => {
-                for (let i = String(str).length; i < 3; i ++) str = " " + str;
-                return str;
-            }
-
-            let farmerString = `\`\`\`   ID   | Pk. | Diff\n--------------------\n`;
-            statusResult.data.farmers.forEach(farmer => {
-                farmerString += `${farmer.launcher_id} | ${formatStr(farmer.points)} | ${formatStr(farmer.difficulty)}\n`;
-            });
-            farmerString += `\`\`\`\n**Count**: ${statusResult.data.count}\n**Total points:** ${statusResult.data.points_total}`;
-
-            console.log(farmerString);
-
+            //farmerString += `\`\`\`\n**Count**: ${statusResult.data.count}\n**Total points:** ${statusResult.data.points_total}`;
+            
             const statusEmbed = {
                 color: botConfig.colors.botColor,
-                title: "Poll Status",
-                description: farmerString,
+                title: 'Pool Status',
+                fields: [
+                    {
+                        name: 'Farmers',
+                        value: statusResult.data.count,
+                        inline: true,
+                    },
+                    {
+                        name: 'Points',
+                        value: statusResult.data.points_total,
+                        inline: true,
+                    },
+                    {
+                        name: 'Netspace',
+                        value: `${statusResult.data.netspace_total} TiB`,
+                        inline: true,
+                    },
+                ],
                 footer: {
                     text: 'XCH Garden',
                 },
                 timestamp: new Date()
-            }
-
+            };
+            
             this.send({embed: statusEmbed});
+
         } catch (error) {
             console.error(error)
             this.sendEmbed(0, "Wystąpił błąd!");
