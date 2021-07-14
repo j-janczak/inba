6d62451052226014751de449c0eac4223301d7e2
k.janczak <kubjan4@gmail.com>
2021-07-14 16:22:18 +0200

    xch garden status udate

--- commands/stats.js
+++ commands/stats.js
@@ -11,12 +11,17 @@ class Stats extends CommandTemplate {
     async getData() {
         try {
             const statusResult = await axios.get(`https://api.xch.garden/stats`);
+            const cryptoResult = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=chia&vs_currencies=pln,usd`);
 
             //farmerString += `\`\`\`\n**Count**: ${statusResult.data.count}\n**Total points:** ${statusResult.data.points_total}`;
             
             const statusEmbed = {
                 color: botConfig.colors.botColor,
-                title: 'Pool Status',
+                author: {
+                    name: 'Pool Status',
+                    icon_url: this.msg.guild.iconURL(),
+                    url: 'https://panel.xch.garden/'
+                },
                 fields: [
                     {
                         name: 'Farmers',
@@ -31,6 +36,11 @@ class Stats extends CommandTemplate {
                     {
                         name: 'Netspace',
                         value: `${statusResult.data.netspace_total} TiB`,
+                        inline: true,
+                    },
+                    {
+                        name: 'Chia value',
+                        value: `${cryptoResult.data.chia.pln} **pln** / **$**${cryptoResult.data.chia.usd}`,
                         inline: true,
                     },
                 ],
@@ -51,5 +61,6 @@ class Stats extends CommandTemplate {
 
 module.exports = {
     name: `stats`,
+    aliases: [`status`],
     execute(msg, args, client) {new Stats(msg, args, client)}
 }
