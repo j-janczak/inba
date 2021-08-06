e320038573833734e1482499a197c181df83b9e9
k.janczak <kubjan4@gmail.com>
2021-08-06 20:35:41 +0200

    fast fix, member add/remove msg

--- inba3.js
+++ inba3.js
@@ -1,8 +1,9 @@
 const { Client, Intents, Collection } = require('discord.js');
+const inbaDB = require('./bot_files/inbaDB.js');
 const { token } = require('./cfg/config.json');
 const fs = require('fs');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
 
 client.commands = new Collection();
 
@@ -14,7 +15,7 @@ for (const file of commandFiles) {
 }
 
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('Ready!d');
 });
 
 client.on('interactionCreate', async interaction => {
@@ -48,5 +49,21 @@ client.on('messageCreate', async message => {
 		});
 	}
 });
+
+async function onGuildMemberAddRemove(guildMember, type) {
+	try {
+		const messagesToSend = (type) ? await inbaDB.get('servers/welcomeMessages', [guildMember.guild.id]) : await inbaDB.get('servers/farewellMessages', [guildMember.guild.id]);
+		if (messagesToSend.data.messages.length > 0) {
+			const mNo = Math.floor(Math.random() * messagesToSend.data.messages.length);
+			const messageToSend = (type) ? messagesToSend.data.messages[mNo].replace('%u', `<@${guildMember.user.id}>`) : messagesToSend.data.messages[mNo].replace('%u', `\`${guildMember.user.tag}\``);
+
+			guildMember.guild.systemChannel.send(messageToSend);
+		}
+	}
+	catch(e) { console.error(e); }
+}
+
+client.on('guildMemberAdd', (guildMember) => { console.log('teset'); onGuildMemberAddRemove(guildMember, 1); });
+client.on('guildMemberRemove', (guildMember) => { onGuildMemberAddRemove(guildMember, 0); });
 
 client.login(token);
